ff68cb9f91eb6da848b0236d5cd017c76b2ccf40
deoreharshal775 <deoreharshal775@gmail.com>
2020-08-08 12:55:43 +0530

    oncreate in ActivityTest

--- app/src/main/java/com/mentebit/deviceinfo/ActivityTest.java
+++ app/src/main/java/com/mentebit/deviceinfo/ActivityTest.java
@@ -1,4 +1,14 @@
 package com.mentebit.deviceinfo;
 
-class ActivityTest {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+class ActivityTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
